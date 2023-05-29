c67286fae255ec1bd72416f947f26e052dec903f
yoshinori1 <KoldunDunDunDun@yandex.ru>
2023-05-29 16:20:45 +0300

    u

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>ТЕХНИЧЕСКАЯ ПОДГОВКА
+    <br>Не менее чем за день до экзамена необходимо выполнить все действия по технической подготовке аудитории.
+</h1>
